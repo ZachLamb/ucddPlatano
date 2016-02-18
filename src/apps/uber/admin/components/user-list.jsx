ca1f3b0041e956235935777444b94e3d6bbbd18d
Zach Lamb <zala3440@colorado.edu>
2016-02-17 21:15:59 -0700

    updated from Z

--- src/apps/uber/admin/components/user-list.jsx
+++ src/apps/uber/admin/components/user-list.jsx
@@ -1,12 +1,34 @@
-class UserList extends React.Component {
-  render(){
-    return (<div>
-      <div> <p>ToDo List of Users</p>
-        <pre> {JSON.stringify(this.props.users)} </pre>
-      </div>      
-    </div>
+MyComponents.User = React.createClass({
+ render: function() {
+   return (
+         <p>{this.props.user}</p>
+   );
+ }
+});
+
+
+MyComponents.UserList = React.createClass({
+  render: function() {
+
+    var usersActive = this.props.users.map(function(u,i){
+      if (u.status === "online"){
+        return <MyComponents.User user={u.username} key={i}/>
+      }
+    })
+    var usersNotActive = this.props.users.map(function(u,i){
+      if (u.status === "offline"){
+        return <MyComponents.User user={u.username} key={i}/>
+      }
+    })
+
+    return (
+      <div>
+      	<h5>Online Users</h5>
+        	{usersActive}
+
+      	<h5>Offline Users</h5>
+        	{usersNotActive}
+      </div>
     );
   }
-}
-
-MyComponents.UserList = UserList
+});
